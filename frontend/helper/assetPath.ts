b510ff00f0a92bc039ed3fb0f62fb685fa3932a0
zerdychhuonchhay <zerdychhuonchhay@gmail.com>
2025-09-05 13:12:21 +0700

    feat: Keep only backend.

--- frontend/helper/assetPath.ts
+++
@@ -1,7 +0,0 @@
-const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || '';
-
-export const getAssetPath = (src: string | undefined) => {
-  if (!src) return "";
-  if (src.startsWith(BASE_PATH)) return src;
-  return `${BASE_PATH}${src}`;
-};
